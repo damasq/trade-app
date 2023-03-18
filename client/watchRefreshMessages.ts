27f9dc301127a106ff94e06432f39f3857776804
dikobrazik <ilnarik@yandex-team.ru>
2023-03-18 19:59:04 +0600

    watcher retries count bounded

--- client/watchRefreshMessages.ts
+++ client/watchRefreshMessages.ts
@@ -1,14 +1,20 @@
+let attemptsLeft = 3;
+
 export function watchRefreshMessages() {
   const wsConnection = new WebSocket('ws://localhost:3000');
 
   wsConnection.onclose = function (event) {
-    setTimeout(watchRefreshMessages, 3000);
+    if (attemptsLeft-- > 0) {
+      setTimeout(watchRefreshMessages, 3000);
+    }
   };
 
   wsConnection.onerror = function (error) {
     console.log('Ошибка ', error);
 
-    setTimeout(watchRefreshMessages, 3000);
+    if (attemptsLeft-- > 0) {
+      setTimeout(watchRefreshMessages, 3000);
+    }
   };
 
   wsConnection.onmessage = function (event) {
