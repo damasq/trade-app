5953cab2e360a522ac5bc914087e2554c4cdaa21
damasq <78789637+damasq@users.noreply.github.com>
2023-03-27 18:19:19 +0300

    entity created, tsconfig edited

---
+++ app/entity/Book.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Book {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    author: string
+
+    @Column()
+    name: string
+
+    @Column()
+    description: string
+}
